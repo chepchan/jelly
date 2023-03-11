6da2052b400d019bf1156d8e21b2889860709eab
margo <f411inv41@gmail.com>
2023-03-11 23:02:14 +0200

    added ebiten

--- jelly.go
+++ jelly.go
@@ -1,10 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-func main() {
-	fmt.Print("Hi jelly :3")
+type Game struct{}
 
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
